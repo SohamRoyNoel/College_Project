e49e377b4af5dc496698b265abe67472e9ad622e
Pewdiepie <noelleroy720@gmail.com>
2018-11-17 02:12:53 +0530

    sidebar fixed

--- index.php
+++ index.php
@@ -96,11 +96,11 @@
     </div>
     <!--/.navbar-collapse-->
     <?php
-            if (isset($_POST['submit'])){
-                $skill = $_POST['skill'];
-                $location = $_POST['location'];
-                header("Location: search.php?skill=$skill&location=$location");
-            }
+    if (isset($_POST['submit'])){
+        $skill = $_POST['skill'];
+        $location = $_POST['location'];
+        header("Location: search.php?skill=$skill&location=$location");
+    }
     ?>
 </nav>
 <div class="banner">
@@ -122,13 +122,13 @@
 <div class="container">
     <div class="grid_1">
         <?php
-            if (isset($_SESSION['confirmQ'])){
-                $val = $_SESSION['confirmQ'];
-                if ($val == "true"){
-                    echo "<script>alert(\"You have successfully applied for the job.\")</script>";
-                    unset($_SESSION['confirmQ']);
-                }
+        if (isset($_SESSION['confirmQ'])){
+            $val = $_SESSION['confirmQ'];
+            if ($val == "true"){
+                echo "<script>alert(\"You have successfully applied for the job.\")</script>";
+                unset($_SESSION['confirmQ']);
             }
+        }
         ?>
         <h3>Featured Employers</h3>
         <ul id="flexiselDemo3">
@@ -171,73 +171,95 @@
     <div class="single">
         <div class="col-md-4">
             <div class="col_3">
-                <h3>Todays Jobs</h3>
+                <h3>Jobs By Companies</h3>
                 <ul class="list_1">
-                    <li><a href="#">Department of Health - Western Australia</a></li>
+                    <?php
+                    $company = "Select * from jobs where type='company'";
+                    $execute = mysqli_query($connection, $company);
+                    while ($row = mysqli_fetch_assoc($execute)) {
+                        $ids = $row['id'];
+                        $titles = $row['title'];
+
+                        ?>
+                        <li><a href="watchJobdetails.php?j=<?php echo $ids; ?>"><?php echo $titles?></a></li>
+                        <?php
+                    }
+                    ?>
                 </ul>
             </div>
             <div class="col_3">
-                <h3>Jobs by Category</h3>
+                <h3>Jobs By Seeker.com</h3>
                 <ul class="list_2">
-                    <li><a href="#">Railway Recruitment</a></li>
+                    <?php
+                    $company = "Select * from jobs where type='creator'";
+                    $execute = mysqli_query($connection, $company);
+                    while ($row = mysqli_fetch_assoc($execute)) {
+                        $ids = $row['id'];
+                        $titles = $row['title'];
+
+                        ?>
+                        <li><a href="watchJobdetails.php?j=<?php echo $ids; ?>"><?php echo $titles?></a></li>
+                        <?php
+                    }
+                    ?>
                 </ul>
             </div>
         </div>
 
         <?php
-                $jobs = "select * from jobs";
-                $query = mysqli_query($connection, $jobs);
-                while ($row = mysqli_fetch_assoc($query)){
-                    $id1 = $row['id'];
-                    $title1 = $row['title'];
-                    $date = $row['date'];
-                    $cid1 = $row['company_id'];
-                    $object1 = $row['object'];
-                    $hremail1 = $row['HRem'];
-                    $hrcontact1 = $row['HRph'];
-                    $salary1 = $row['salary'];
-                    $location1 = $row['location'];
-                    $target1 = $row['targetTo'];
-                    $skill1 = $row['skill'];
-                    $education1 = $row['mineducation'];
-                    $role1 = $row['role'];
+        $jobs = "select * from jobs";
+        $query = mysqli_query($connection, $jobs);
+        while ($row = mysqli_fetch_assoc($query)){
+            $id1 = $row['id'];
+            $title1 = $row['title'];
+            $date = $row['date'];
+            $cid1 = $row['company_id'];
+            $object1 = $row['object'];
+            $hremail1 = $row['HRem'];
+            $hrcontact1 = $row['HRph'];
+            $salary1 = $row['salary'];
+            $location1 = $row['location'];
+            $target1 = $row['targetTo'];
+            $skill1 = $row['skill'];
+            $education1 = $row['mineducation'];
+            $role1 = $row['role'];
 
-                    $company = "select * from company where id = {$cid1}";
-                    $cquery = mysqli_query($connection, $company);
-                    while ($row = mysqli_fetch_assoc($cquery)){
-                        $db_company_id = $row['id'];
-                        $db_company_name = $row['name'];
-                        $db_company_reg = $row['reg'];
-                        $db_company_iso = $row['iso'];
-                        $db_company_em = $row['email'];
-                        $db_company_dob = $row['since'];
-                        $db_company_img = $row['img'];
-                        $db_company_password = $row['password'];
-                    }
-        ?>
+            $company = "select * from company where id = {$cid1}";
+            $cquery = mysqli_query($connection, $company);
+            while ($row = mysqli_fetch_assoc($cquery)){
+                $db_company_id = $row['id'];
+                $db_company_name = $row['name'];
+                $db_company_reg = $row['reg'];
+                $db_company_iso = $row['iso'];
+                $db_company_em = $row['email'];
+                $db_company_dob = $row['since'];
+                $db_company_img = $row['img'];
+                $db_company_password = $row['password'];
+            }
+            ?>
 
-        <div class="col-md-8 pull-right">
-            <div class="col_1">
-                <div class="col-sm-4 row_2">
-                    <a href="watchJobdetails.php?j=<?php echo $id1; ?>"><img src="companyImage/<?php echo $db_company_img; ?>" class="img-responsive" alt=""/></a>
-                </div>
-                <div class="col-sm-8 row_1">
-                    <h4><a href="watchJobdetails.php?j=<?php echo $id1; ?>"><?php echo $title1; ?></a></h4>
-                    <h6>Posted On <span class="dot">·</span> <?php echo $date; ?></h6>
-                    <p><?php echo $object1; ?></p>
-                    <div class="social">
-                        <a class="btn_1" href="watchJobdetails.php?j=<?php echo $id1; ?>">
-                            <i class="fa fa-eye"></i>
-                            <span class="share1 fb">Watch Details</span>
-                        </a>
+            <div class="col-md-8 pull-right">
+                <div class="col_1">
+                    <div class="col-sm-4 row_2">
+                        <a href="watchJobdetails.php?j=<?php echo $id1; ?>"><img src="companyImage/<?php echo $db_company_img; ?>" class="img-responsive" alt=""/></a>
                     </div>
+                    <div class="col-sm-8 row_1">
+                        <h4><a href="watchJobdetails.php?j=<?php echo $id1; ?>"><?php echo $title1; ?></a></h4>
+                        <h6>Posted On <span class="dot">·</span> <?php echo $date; ?></h6>
+                        <p><?php echo $object1; ?></p>
+                        <div class="social">
+                            <a class="btn_1" href="watchJobdetails.php?j=<?php echo $id1; ?>">
+                                <i class="fa fa-eye"></i>
+                                <span class="share1 fb">Watch Details</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
                 </div>
-                <div class="clearfix"> </div>
             </div>
-        </div>
 
 
-    <?php } ?>
+        <?php } ?>
 
         <div class="clearfix"> </div>
     </div>
--- watchJobdetails.php
+++ watchJobdetails.php
@@ -45,20 +45,7 @@ if (isset($_GET['j'])){
 
 
 <div class="single">
-    <div class="col-md-4">
-        <div class="col_3">
-            <h3>Todays Jobs</h3>
-            <ul class="list_1">
-                <li><a href="#">Department of Health - Western Australia</a></li>
-            </ul>
-        </div>
-        <div class="col_3">
-            <h3>Jobs by Category</h3>
-            <ul class="list_2">
-                <li><a href="#">Railway Recruitment</a></li>
-            </ul>
-        </div>
-    </div>
+    <?php include "includes/sidebar.php"?>
 
     <div class="col-md-8 single_right">
         <?php
